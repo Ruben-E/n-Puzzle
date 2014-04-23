95909e8bfaa5ab7eca075aa49a766d0b69b83ef8
Ruben Ernst <rubenernst@gmail.com>
2014-04-23 19:04:45 +0200

    Implemented buttons in MainMenuActivity

--- nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MainMenuActivity.java
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MainMenuActivity.java
@@ -1,17 +1,41 @@
 package nl.rubenernst.han.mad.android.puzzle;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class MainMenuActivity extends ActionBarActivity {
+public class MainMenuActivity extends ActionBarActivity implements View.OnClickListener {
+
+    @InjectView(R.id.singleplayer_button)
+    Button singleplayerButton;
+
+    @InjectView(R.id.multiplayer_button)
+    Button multiplayerButton;
+
+    @InjectView(R.id.awards_button)
+    Button awardsButton;
+
+    @InjectView(R.id.scores_button)
+    Button scoresButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        ButterKnife.inject(this);
+
+        singleplayerButton.setOnClickListener(this);
+        multiplayerButton.setOnClickListener(this);
+        awardsButton.setOnClickListener(this);
+        scoresButton.setOnClickListener(this);
     }
 
 
@@ -35,4 +59,22 @@ public class MainMenuActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.singleplayer_button:
+                Intent intent = new Intent(this, GameSelectionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.multiplayer_button:
+
+                break;
+            case R.id.awards_button:
+
+                break;
+            case R.id.scores_button:
+
+                break;
+        }
+    }
 }
