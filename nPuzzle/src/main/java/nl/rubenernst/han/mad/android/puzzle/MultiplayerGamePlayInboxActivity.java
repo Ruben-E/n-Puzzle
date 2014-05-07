b24ebcf308ab053619f9071bea53171004862063
Ruben Ernst <rubenernst@gmail.com>
2014-05-07 15:53:30 +0200

    Created a file for handling the invites through the inbox

---
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MultiplayerGamePlayInboxActivity.java
@@ -0,0 +1,8 @@
+package nl.rubenernst.han.mad.android.puzzle;
+
+/**
+ * Created by rubenernst on 07-05-14.
+ */
+public class MultiplayerGamePlayInboxActivity extends MultiplayerGamePlayActivity {
+
+}
