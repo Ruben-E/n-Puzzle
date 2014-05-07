e4ce245f23fccf7e05c07425a8ec97812f6fbc81
Ruben Ernst <rubenernst@gmail.com>
2014-05-07 16:06:07 +0200

    Implement inbox to select existing matches or invites

--- nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MultiplayerGamePlayInboxActivity.java
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MultiplayerGamePlayInboxActivity.java
@@ -1,8 +1,64 @@
 package nl.rubenernst.han.mad.android.puzzle;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesStatusCodes;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.turnbased.OnTurnBasedMatchUpdateReceivedListener;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatch;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
+
+import java.util.ArrayList;
+
 /**
  * Created by rubenernst on 07-05-14.
  */
 public class MultiplayerGamePlayInboxActivity extends MultiplayerGamePlayActivity {
+    private static final String TAG = "Multiplayer";
 
+    private static final int RC_LOOK_AT_MATCHES = 10001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (isSignedIn()) {
+            SelectInbox();
+        } else {
+            beginUserInitiatedSignIn();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int request, int response, Intent data) {
+        super.onActivityResult(request, response, data);
+
+        if (request == RC_LOOK_AT_MATCHES) {
+            if (response != Activity.RESULT_OK) {
+                return;
+            }
+
+            mMatch = data.getParcelableExtra(Multiplayer.EXTRA_TURN_BASED_MATCH);
+            launchMatch();
+        }
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        super.onSignInSucceeded();
+
+        SelectInbox();
+    }
+
+    public void SelectInbox() {
+        Intent intent = Games.TurnBasedMultiplayer.getInboxIntent(getApiClient());
+        startActivityForResult(intent, RC_LOOK_AT_MATCHES);
+    }
 }
