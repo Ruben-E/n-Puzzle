b410e3bbe08f9e46165bfcce5fd57c5a22e651d0
Ruben Ernst <rubenernst@gmail.com>
2014-05-07 15:54:41 +0200

    Created a file for handling a game game via inviting a player

---
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MultiplayerGamePlayPlayerSelectionActivity.java
@@ -0,0 +1,7 @@
+package nl.rubenernst.han.mad.android.puzzle;
+
+/**
+ * Created by rubenernst on 07-05-14.
+ */
+public class MultiplayerGamePlayPlayerSelectionActivity extends MultiplayerGamePlayActivity {
+}
