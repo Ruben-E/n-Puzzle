0c8a4170918c79b1f8124643d292bf544d3d9b15
Ruben Ernst <rubenernst@gmail.com>
2014-06-07 11:43:46 +0200

    Implemented a slider pager as main menu

--- nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MainMenuActivity.java
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MainMenuActivity.java
@@ -2,8 +2,13 @@ package nl.rubenernst.han.mad.android.puzzle;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,31 +16,40 @@ import android.view.Window;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import com.astuetz.PagerSlidingTabStrip;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
 import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
 import com.google.example.games.basegameutils.BaseGameActivity;
+import nl.rubenernst.han.mad.android.puzzle.fragments.MainMenuFragment;
 
 import java.util.ArrayList;
 
 
 public class MainMenuActivity extends BaseGameActivity implements View.OnClickListener {
 
-    @InjectView(R.id.singleplayer_button)
-    Button singleplayerButton;
+//    @InjectView(R.id.singleplayer_button)
+//    Button singleplayerButton;
+//
+//    @InjectView(R.id.multiplayer_new_game_button)
+//    Button multiplayerNewGameButton;
+//
+//    @InjectView(R.id.multiplayer_existing_game_button)
+//    Button multiplayerExistingGameButton;
+//
+//    @InjectView(R.id.awards_button)
+//    Button awardsButton;
+//
+//    @InjectView(R.id.scores_button)
+//    Button scoresButton;
 
-    @InjectView(R.id.multiplayer_new_game_button)
-    Button multiplayerNewGameButton;
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+    private MainMenuPagerAdapter adapter;
 
-    @InjectView(R.id.multiplayer_existing_game_button)
-    Button multiplayerExistingGameButton;
-
-    @InjectView(R.id.awards_button)
-    Button awardsButton;
-
-    @InjectView(R.id.scores_button)
-    Button scoresButton;
+    public MainMenuActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +58,23 @@ public class MainMenuActivity extends BaseGameActivity implements View.OnClickLi
 
         ButterKnife.inject(this);
 
-        singleplayerButton.setOnClickListener(this);
-        multiplayerNewGameButton.setOnClickListener(this);
-        multiplayerExistingGameButton.setOnClickListener(this);
-        awardsButton.setOnClickListener(this);
-        scoresButton.setOnClickListener(this);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+        adapter = new MainMenuPagerAdapter(getSupportFragmentManager());
+
+        pager.setAdapter(adapter);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
+
+        tabs.setViewPager(pager);
+
+//        singleplayerButton.setOnClickListener(this);
+//        multiplayerNewGameButton.setOnClickListener(this);
+//        multiplayerExistingGameButton.setOnClickListener(this);
+//        awardsButton.setOnClickListener(this);
+//        scoresButton.setOnClickListener(this);
     }
 
 
@@ -74,35 +100,35 @@ public class MainMenuActivity extends BaseGameActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
-        Intent intent;
-        switch (view.getId()) {
-            case R.id.singleplayer_button:
-                intent = new Intent(this, GameSelectionActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.multiplayer_new_game_button:
-                intent = new Intent(this, MultiplayerGamePlayPlayerSelectionActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.multiplayer_existing_game_button:
-                intent = new Intent(this, MultiplayerGamePlayInboxActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.awards_button:
-                if (isSignedIn()) {
-                    startActivityForResult(Games.Achievements.getAchievementsIntent(getApiClient()), 0);
-                } else {
-                    showAlert("Achievements not available");
-                }
-                break;
-            case R.id.scores_button:
-                if (isSignedIn()) {
-                    startActivityForResult(Games.Leaderboards.getAllLeaderboardsIntent(getApiClient()), 0);
-                } else {
-                    showAlert("Leaderboards not available");
-                }
-                break;
-        }
+//        Intent intent;
+//        switch (view.getId()) {
+//            case R.id.singleplayer_button:
+//                intent = new Intent(this, GameSelectionActivity.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.multiplayer_new_game_button:
+//                intent = new Intent(this, MultiplayerGamePlayPlayerSelectionActivity.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.multiplayer_existing_game_button:
+//                intent = new Intent(this, MultiplayerGamePlayInboxActivity.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.awards_button:
+//                if (isSignedIn()) {
+//                    startActivityForResult(Games.Achievements.getAchievementsIntent(getApiClient()), 0);
+//                } else {
+//                    showAlert("Achievements not available");
+//                }
+//                break;
+//            case R.id.scores_button:
+//                if (isSignedIn()) {
+//                    startActivityForResult(Games.Leaderboards.getAllLeaderboardsIntent(getApiClient()), 0);
+//                } else {
+//                    showAlert("Leaderboards not available");
+//                }
+//                break;
+//        }
     }
 
     @Override
@@ -112,6 +138,31 @@ public class MainMenuActivity extends BaseGameActivity implements View.OnClickLi
 
     @Override
     public void onSignInSucceeded() {
+
+    }
+
+    public class MainMenuPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = { "Home", "Your matches", "High Scores", "Achievements" };
+
+        public MainMenuPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MainMenuFragment.newInstance();
+        }
 
     }
 }
---
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/fragments/MainMenuFragment.java
@@ -0,0 +1,101 @@
+package nl.rubenernst.han.mad.android.puzzle.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.Button;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import nl.rubenernst.han.mad.android.puzzle.GameSelectionActivity;
+import nl.rubenernst.han.mad.android.puzzle.MultiplayerGamePlayInboxActivity;
+import nl.rubenernst.han.mad.android.puzzle.MultiplayerGamePlayPlayerSelectionActivity;
+import nl.rubenernst.han.mad.android.puzzle.R;
+
+public class MainMenuFragment extends Fragment implements View.OnClickListener {
+
+    @InjectView(R.id.singleplayer_button)
+    Button singleplayerButton;
+
+    @InjectView(R.id.multiplayer_new_game_button)
+    Button multiplayerNewGameButton;
+
+    @InjectView(R.id.multiplayer_existing_game_button)
+    Button multiplayerExistingGameButton;
+
+    @InjectView(R.id.awards_button)
+    Button awardsButton;
+
+    @InjectView(R.id.scores_button)
+    Button scoresButton;
+
+    public static MainMenuFragment newInstance() {
+        return new MainMenuFragment();
+    }
+
+    public MainMenuFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        ButterKnife.inject(this, rootView);
+
+        singleplayerButton.setOnClickListener(this);
+        multiplayerNewGameButton.setOnClickListener(this);
+        multiplayerExistingGameButton.setOnClickListener(this);
+        awardsButton.setOnClickListener(this);
+        scoresButton.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        ButterKnife.reset(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.singleplayer_button:
+                intent = new Intent(getActivity(), GameSelectionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.multiplayer_new_game_button:
+                intent = new Intent(getActivity(), MultiplayerGamePlayPlayerSelectionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.multiplayer_existing_game_button:
+                intent = new Intent(getActivity(), MultiplayerGamePlayInboxActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.awards_button:
+//                if (isSignedIn()) {
+//                    startActivityForResult(Games.Achievements.getAchievementsIntent(getApiClient()), 0);
+//                } else {
+//                    showAlert("Achievements not available");
+//                }
+                break;
+            case R.id.scores_button:
+//                if (isSignedIn()) {
+//                    startActivityForResult(Games.Leaderboards.getAllLeaderboardsIntent(getApiClient()), 0);
+//                } else {
+//                    showAlert("Leaderboards not available");
+//                }
+                break;
+        }
+    }
+}
