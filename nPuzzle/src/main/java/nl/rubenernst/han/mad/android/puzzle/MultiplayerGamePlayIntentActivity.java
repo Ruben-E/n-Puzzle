a44d8870ead8081f91256b75d99cb4268d88639f
Ruben Ernst <rubenernst@gmail.com>
2014-06-08 12:10:27 +0200

    Created an activity to start a match by an intent

---
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/MultiplayerGamePlayIntentActivity.java
@@ -0,0 +1,7 @@
+package nl.rubenernst.han.mad.android.puzzle;
+
+/**
+ * Created by rubenernst on 08-06-14.
+ */
+public class MultiplayerGamePlayIntentActivity extends MultiplayerGamePlayActivity {
+}
