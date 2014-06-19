eb95eed0f056fe6aff38cadf02b6fbe6e4aa2124
Ruben Ernst <rubenernst@gmail.com>
2014-06-19 12:27:19 +0200

    Created a GamePlayStatusViewAdapter interface to get the status view for a game

---
+++ nPuzzle/src/main/java/nl/rubenernst/han/mad/android/puzzle/interfaces/GamePlayStatusViewAdapter.java
@@ -0,0 +1,10 @@
+package nl.rubenernst.han.mad.android.puzzle.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by rubenernst on 19-06-14.
+ */
+public interface GamePlayStatusViewAdapter {
+    public View getStatusView();
+}
